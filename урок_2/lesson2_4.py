73acc8397d8bc3dc530914b2139e306625b639f7
Ihor676 <94188738+Ihor676@users.noreply.github.com>
2021-11-14 21:24:47 +0300

    Update lesson2_4.py

--- урок_2/lesson2_4.py
+++ урок_2/lesson2_4.py
@@ -1,10 +1,9 @@
 print("Привет меня зовут Жарвис", end= " ")
 name = input("а тебя как зовут?")
 print("Как дела", name, "?")
-n1 = int(input())
-n2= int(input())
-n3= int(input(n1+n2))
-
+n1 = int(input("Загадай целое число:"))
+n2 = int(input("Загадай второе целое:"))
+print("Сума этих чисел", n1+n2 ,)
 
 
 
